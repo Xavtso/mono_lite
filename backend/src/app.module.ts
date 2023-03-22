513ea60bd9b83c0a51c8c5d1646fa060fff005e2
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-03-22 09:50:21 +0200

    connected Azure Database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,7 +9,8 @@ import { AccountsModule } from './accounts/accounts.module';
 import { CardsService } from './cards/cards.service';
 import { CardsModule } from './cards/cards.module';
 import { Card } from './cards/card.model';
-
+import * as mssql from 'mssql';
+import * as tedious from 'tedious'
 
 @Module({
   controllers: [AppController],
@@ -19,17 +20,31 @@ import { Card } from './cards/card.model';
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: String(process.env.POSTGRES_PASSWORD),
-      database: process.env.POSTGRES_DB,
+      dialect: 'mssql',
+      dialectModule: tedious,
+      username: 'CloudSA08c87698',
+      password: 'vh2004r44VH',
+      host: 'monolite.database.windows.net',
+      port: 1433,
+      database: 'mono_db',
+      dialectOptions: {
+        driver: {
+          version: 'ODBC Driver 18 for SQL Server',
+        },
+        options: {
+          encrypt: true,
+          database: 'mono_db',
+          authentication: {
+            type: 'azure-active-directory-msi-app-service'
+          }
+        },
+        encrypt: true,
+        trustServerCertificate: false,
+      },
       models: [User, Card],
       autoLoadModels: true,
     }),
     UsersModule,
-    AccountsModule,
     CardsModule,
   ],
 })
